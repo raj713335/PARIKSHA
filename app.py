d885cabbb7cd6a28ddc76cfc18eec69016951933
Arnab Das <arnab.das@orchids.edu.in>
2021-02-06 20:00:36 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "<h1>Hello Puppy!</h1>"
+
+
+@app.route('/information')
+def info():
+    return "<h1>Puppies are cute!</h1>"
+
+
+@app.route('/puppy/<name>')
+def puppy(name):
+    return "<h1>This is a page for {}</h1>".format(name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
