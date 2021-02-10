bb49ffec7820ff90115710fd614911f63f4e0dfe
Arnab Das <arnab.das@orchids.edu.in>
2021-02-10 19:26:25 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,57 +1,39 @@
 import sys
-import platform
-from PySide2 import QtCore, QtGui, QtWidgets
-from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
-from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PySide2 import QtCore
+from PySide2.QtGui import (QColor)
 from PySide2.QtWidgets import *
 
-## ==> SPLASH SCREEN
+# SPLASH SCREEN
 from ui_splash_screen import Ui_SplashScreen
 
-## ==> MAIN WINDOW
-from ui_main import Ui_MainWindow
 
-## ==> GLOBALS
+# GLOBALS
 counter = 0
 
-# YOUR APPLICATION
-class MainWindow(QMainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
 
-        # MAIN WINDOW LABEL
-        QtCore.QTimer.singleShot(1500, lambda: self.ui.label.setText("<strong>THANKS</strong> FOR WATCHING"))
-        QtCore.QTimer.singleShot(1500, lambda: self.setStyleSheet("background-color: #222; color: #FFF"))
-
-
-# SPLASH SCREEN
+# LOADING SCREEN
 class SplashScreen(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.ui = Ui_SplashScreen()
         self.ui.setupUi(self)
 
-        ## UI ==> INTERFACE CODES
-        ########################################################################
-
-        ## REMOVE TITLE BAR
+        # REMOVE TITLE BAR
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
 
-
-        ## DROP SHADOW EFFECT
+        # DROP SHADOW EFFECT
         self.shadow = QGraphicsDropShadowEffect(self)
         self.shadow.setBlurRadius(20)
         self.shadow.setXOffset(0)
         self.shadow.setYOffset(0)
         self.shadow.setColor(QColor(0, 0, 0, 60))
-        self.ui.DropShadowFrame.setGraphicsEffect(self.shadow)
+        self.ui.dropShadowFrame.setGraphicsEffect(self.shadow)
 
-        ## QTIMER ==> START
+        # TIMER START
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.progress)
+
         # TIMER IN MILLISECONDS
         self.timer.start(35)
 
@@ -62,16 +44,14 @@ class SplashScreen(QMainWindow):
 
         # Change Texts
         QtCore.QTimer.singleShot(1500, lambda: self.ui.label_description.setText("<strong>LOADING</strong> DATABASE"))
-        QtCore.QTimer.singleShot(3000, lambda: self.ui.label_description.setText("<strong>LOADING</strong> USER INTERFACE"))
+        QtCore.QTimer.singleShot(3000, lambda: self.ui.label_description.setText("<strong>"
+                                                                                 "LOADING</strong> USER INTERFACE"))
 
-
-        ## SHOW ==> MAIN WINDOW
-        ########################################################################
+        # SHOW MAIN WINDOW
         self.show()
-        ## ==> END ##
+        # END
 
-    ## ==> APP FUNCTIONS
-    ########################################################################
+    # APP FUNCTIONS
     def progress(self):
 
         global counter
@@ -85,16 +65,13 @@ class SplashScreen(QMainWindow):
             self.timer.stop()
 
             # SHOW MAIN WINDOW
-            self.main = MainWindow()
-            self.main.show()
+            # ADD FURTHER
 
             # CLOSE SPLASH SCREEN
             self.close()
 
         # INCREASE COUNTER
         counter += 1
-
-
 
 
 if __name__ == "__main__":
